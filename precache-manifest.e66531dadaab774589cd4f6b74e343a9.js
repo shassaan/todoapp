c29e5495a31df3e1d958fa0827ad2fa903cbbc20
shassaan <syedhassaan.103@gmail.com>
2020-03-21 21:45:34 +0500

    Updates

---
+++ precache-manifest.e66531dadaab774589cd4f6b74e343a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fc56e647cdc893cb2fdb7925c46cd4",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "d2b081e4570cb538f24c",
+    "url": "/todoapp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ffcbf3b920ab5b533a97",
+    "url": "/todoapp/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "d2b081e4570cb538f24c",
+    "url": "/todoapp/static/js/2.5325d657.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todoapp/static/js/2.5325d657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcbf3b920ab5b533a97",
+    "url": "/todoapp/static/js/main.eaf9c264.chunk.js"
+  },
+  {
+    "revision": "dc1346c9dd92e8b0633b",
+    "url": "/todoapp/static/js/runtime-main.30031cea.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/todoapp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
