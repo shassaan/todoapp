bc011832d61671a101f896972e3a07979edda4c3
shassaan <syedhassaan.103@gmail.com>
2020-03-21 21:19:08 +0500

    Updates

--- src/Context/TaskContext.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const TaskContext = React.createContext(0);
--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = ()=>{
-    return(
-        <h1 className="jumbotron text-center"> <span className="text text-info">To</span> Do App</h1>
-    );
-}
-
-export default Header;
